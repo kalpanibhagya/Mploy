79f3ac5d1f43a3108183a1f11f03f447e7280a2d
janithR <rumalkajanith@gmail.com>
2017-12-04 23:18:38 +0530

    add applicant profile loging name

--- application/views/Pages/Applicant/profile.php
+++ application/views/Pages/Applicant/profile.php
@@ -62,7 +62,7 @@
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <img src="<?php echo base_url()."assets/AdminLTE/"; ?>dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                            <span class="hidden-xs">Alexander Pierce</span>
+                            <span class="hidden-xs"><?php echo $username?></span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- User image -->
@@ -70,7 +70,7 @@
                                 <img src="<?php echo base_url()."assets/AdminLTE/"; ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                                 <p>
-                                    Alexander Pierce - Web Developer
+                                    <?php echo $username?> - Web Developer
                                     <small>Member since Nov. 2012</small>
                                 </p>
                             </li>
@@ -99,7 +99,7 @@
                     <img src="<?php echo base_url()."assets/AdminLTE/"; ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
-                    <p>Alexander Pierce</p>
+                    <p><?php echo $username?></p>
                     <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
                 </div>
             </div>
