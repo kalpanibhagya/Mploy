49cdce9dbacd859f9c0b81dae9b2039e1d44b3ab
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-12-04 20:09:56 +0530

    Changes due to modifications in the database

--- application/views/Pages/Applicant/profile.php
+++ application/views/Pages/Applicant/profile.php
@@ -155,7 +155,7 @@
 
                         <h3  class="profile-username text-center"><b><?php echo $username?></b></h3>
 
-                        <h3 class="profile-username text-center"><?php echo $first_name. $last_name?></h3>
+                        <h3 class="profile-username text-center"><?php echo $full_name?></h3>
 
                         <p class="text-muted text-center">Software Engineer</p>
                         <p class="text-muted text-center"><?php echo $dob ?></p>
