3461fac8af00cb1032a9a809f4424ecd6b2f7f96
janithR <rumalkajanith@gmail.com>
2017-12-05 21:46:07 +0530

    Get all data from applicant

--- application/views/Pages/Applicant/dashboard.php
+++ application/views/Pages/Applicant/dashboard.php
@@ -62,7 +62,7 @@
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <img src="<?php echo base_url()."assets/AdminLTE/"; ?>dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                            <span class="hidden-xs">Alexander Pierce</span>
+                            <span class="hidden-xs"><?php echo $username ?></span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- User image -->
@@ -70,7 +70,7 @@
                                 <img src="<?php echo base_url()."assets/AdminLTE/"; ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                                 <p>
-                                    Alexander Pierce - Web Developer
+                                    <?php echo $full_name ?> - Web Developer
                                     <small>Member since Nov. 2012</small>
                                 </p>
                             </li>
@@ -99,7 +99,7 @@
                     <img src="<?php echo base_url()."assets/AdminLTE/"; ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
-                    <p>Alexander Pierce</p>
+                    <p><?php echo $username ?></p>
                 </div>
             </div>
             <!-- sidebar menu: : style can be found in sidebar.less -->
