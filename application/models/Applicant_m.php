da33c20d4fe0abd22bd7ce6c435d8f34630d4313
janithR <rumalkajanith@gmail.com>
2017-12-05 21:47:00 +0530

    Add dash board applicant name

--- application/models/Applicant_m.php
+++ application/models/Applicant_m.php
@@ -20,13 +20,13 @@ class Applicant_m extends CI_Model{
 
     function get_data($email)
     {
-        $this->db->select('username, full_name, dob, gender');
+
         $this->db->where('email', $email);
         $query = $this->db->get('applicant');
         $result = $query->row();
 
 
-        $data = array('username'=> ($result->username), 'full_name'=> ($result->full_name), 'dob'=>($result->dob), 'gender'=>($result->gender));
+        $data = array('username'=> ($result->username), 'full_name'=> ($result->full_name), 'dob'=>($result->dob), 'gender'=>($result->gender), 'profile_image'=>($result->profile_image), 'cover_image'=>($result->cover_image), 'email'=>($result->email), 'contact'=>($result->contact), 'address'=>($result->address), 'applicant_type'=>($result->applicant_type), 'preffered_area'=>($result->preffered_area), 'company_one'=>($result->company_one), 'company_two'=>($result->company_two), 'company_three'=>($result->company_three), 'password'=>($result->password));
 
         return $data;
 
