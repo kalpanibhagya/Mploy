065b6fc2751b6a0aea66654772c59cb6bdacc8f1
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-12-06 02:46:07 +0530

    This fixes #4

--- application/views/Pages/Company/content.php
+++ application/views/Pages/Company/content.php
@@ -1,6 +1,6 @@
 <div id="haha">
     <div class="row">
-        <h4 style="text-align: center;font-weight: bolder">Jobs Posted by Others</h4>
+        <h4 style="text-align: center;font-weight: bolder">Jobs</h4>
         <div class="input-group">
             <input type="text" class="search form-control" placeholder="Search for: Internships, Jobs, Job Titles, City or Country" width="50px">
             <span class="input-group-btn">
