16c8caa12178384926961a568e7a6d1f3c1a6bc3
janithR <rumalkajanith@gmail.com>
2017-12-06 11:31:44 +0530

    Clear merge errors

--- application/views/Pages/Applicant/dashboard.php
+++ application/views/Pages/Applicant/dashboard.php
@@ -64,90 +64,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <!-- Navbar Right Menu -->
             <div class="navbar-custom-menu">
                 <ul class="nav navbar-nav">
-<<<<<<< HEAD
-                    <!-- User Account: style can be found in dropdown.less -->
-                    <li class="dropdown user user-menu">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <img src="<?php echo base_url()."assets/AdminLTE/"; ?>dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                            <span class="hidden-xs"><?php echo $username ?></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <!-- User image -->
-                            <li class="user-header">
-                                <img src="<?php echo base_url()."assets/AdminLTE/"; ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
-                                <p>
-                                    <?php echo $full_name ?> - Web Developer
-                                    <small>Member since Nov. 2012</small>
-                                </p>
-                            </li>
-                            <!-- Menu Footer-->
-                            <li class="user-footer">
-                                <div class="pull-left">
-                                    <a href="<?php echo base_url(); ?>Applicant/profile" class="btn btn-default btn-flat">Profile</a>
-                                </div>
-                                <div class="pull-right">
-                                    <a href="<?php echo base_url(); ?>Applicant/logout" class="btn btn-default btn-flat">Sign out</a>
-                                </div>
-                            </li>
-                        </ul>
-=======
-
-
                     <!-- User Account Menu -->
                     <li class="">
                         <!-- Menu Toggle Button -->
                         <a href="<?php echo site_url('Applicant/logout') ?>"> <i class="fa fa-sign-out"></i>
                             Log out </a>
 
->>>>>>> master
                     </li>
                 </ul>
             </div>
         </nav>
     </header>
-<<<<<<< HEAD
-    <!-- Left side column. contains the logo and sidebar -->
-    <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-            <!-- Sidebar user panel -->
-            <div class="user-panel">
-                <div class="pull-left image">
-                    <img src="<?php echo base_url()."assets/AdminLTE/"; ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-                </div>
-                <div class="pull-left info">
-                    <p><?php echo $username ?></p>
-                </div>
-            </div>
-            <!-- sidebar menu: : style can be found in sidebar.less -->
-            <ul class="sidebar-menu" data-widget="tree">
-                <li class="active">
-                    <a href="#">
-                        <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="<?php echo base_url(); ?>Applicant/interviewRequests">
-                        <i class="fa fa-folder"></i> <span>Interview Requests</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="<?php echo base_url(); ?>Applicant/employers">
-                        <i class="fa fa-dashboard"></i> <span>Companies</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="<?php echo base_url(); ?>Applicant/notifications">
-                        <i class="fa fa-bell-o"></i> <span>Notifications</span>
-                    </a>
-                </li>
-            </ul>
-        </section>
-        <!-- /.sidebar -->
-    </aside>
-=======
->>>>>>> master
+
+
 
 
     <?php $this->load->view('Pages/Applicant/navigation_bar');?>
--- application/views/Pages/Applicant/profile.php
+++ application/views/Pages/Applicant/profile.php
@@ -1,108 +1,4 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Dashboard</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="<?php echo base_url()."assets/AdminLTE/"; ?>bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?php echo base_url()."assets/AdminLTE/"; ?>bower_components/font-awesome/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="<?php echo base_url()."assets/AdminLTE/"; ?>bower_components/Ionicons/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="<?php echo base_url()."assets/AdminLTE/"; ?>dist/css/AdminLTE.min.css">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="<?php echo base_url()."assets/AdminLTE/"; ?>dist/css/skins/_all-skins.min.css">
-    <!-- Morris chart -->
-    <link rel="stylesheet" href="<?php echo base_url()."assets/AdminLTE/"; ?>bower_components/morris.js/morris.css">
-    <!-- jvectormap -->
-    <link rel="stylesheet" href="<?php echo base_url()."assets/AdminLTE/"; ?>bower_components/jvectormap/jquery-jvectormap.css">
-    <!-- Date Picker -->
-    <link rel="stylesheet" href="<?php echo base_url()."assets/AdminLTE/"; ?>bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="<?php echo base_url()."assets/AdminLTE/"; ?>bower_components/bootstrap-daterangepicker/daterangepicker.css">
-    <!-- bootstrap wysihtml5 - text editor -->
-    <link rel="stylesheet" href="<?php echo base_url()."assets/AdminLTE/"; ?>plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-</head>
-<body class="hold-transition skin-green sidebar-mini">
-<div class="wrapper">
-
-    <header class="main-header">
-        <!-- Logo -->
-        <a href="<?php echo base_url(); ?>Welcome/Company_main" class="logo">
-            <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>M</b></span>
-            <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>M-Ploy</b></span>
-        </a>
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top">
-            <!-- Sidebar toggle button-->
-            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-                <span class="sr-only">Toggle navigation</span>
-            </a>
-
-            <div class="navbar-custom-menu">
-                <ul class="nav navbar-nav">
-                    <!-- User Account: style can be found in dropdown.less -->
-                    <li class="dropdown user user-menu">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <img src="<?php echo base_url()."assets/AdminLTE/"; ?>dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                            <span class="hidden-xs"><?php echo $username?></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <!-- User image -->
-                            <li class="user-header">
-                                <img src="<?php echo base_url()."assets/AdminLTE/"; ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
-                                <p>
-                                    <?php echo $username?> - Web Developer
-                                    <small>Member since Nov. 2012</small>
-                                </p>
-                            </li>
-                            <!-- Menu Footer-->
-                            <li class="user-footer">
-                                <div class="pull-left">
-                                    <a href="<?php echo base_url(); ?>Applicant/profile" class="btn btn-default btn-flat">Profile</a>
-                                </div>
-                                <div class="pull-right">
-                                    <a href="<?php echo base_url(); ?>Applicant/logout" class="btn btn-default btn-flat">Sign out</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-    <!-- Left side column. contains the logo and sidebar -->
-    <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-            <!-- Sidebar user panel -->
-            <div class="user-panel">
-                <div class="pull-left image">
-                    <img src="<?php echo base_url()."assets/AdminLTE/"; ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-                </div>
-                <div class="pull-left info">
-                    <p><?php echo $username?></p>
-                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-=======
 <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
 <link href="<?php echo base_url('assets/datatables/css/dataTables.bootstrap.css')?>" rel="stylesheet">
 <link href="<?php echo base_url('assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css')?>" rel="stylesheet">
@@ -121,6 +17,7 @@
             <!-- Profile Image -->
             <div class="box box-primary">
                 <div class="box-body box-profile">
+                    <a class="glyphicon glyphicon-pencil" role="button"></a>
                     <img class="profile-user-img img-responsive img-square" src="#" alt="User profile picture" style="height: 200px;width: 200px">
 
                     <h3  class="profile-username text-center"><b><?php echo $username?></b></h3>
@@ -130,93 +27,20 @@
                     <p class="text-muted text-center">Software Engineer</p>
                     <p class="text-muted text-center"><?php echo $dob ?></p>
                     <p class="text-muted text-center"><?php echo $gender ?></p>
->>>>>>> master
+
                 </div>
                 <!-- /.box-body -->
             </div>
-<<<<<<< HEAD
-            <!-- sidebar menu: : style can be found in sidebar.less -->
-            <ul class="sidebar-menu" data-widget="tree">
-                <li>
-                    <a href="<?php echo base_url(); ?>Applicant/dashboard">
-                        <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="<?php echo base_url(); ?>Applicant/interviewRequests">
-                        <i class="fa fa-folder"></i> <span>Interview Requests</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="<?php echo base_url(); ?>Applicant/employers">
-                        <i class="fa fa-dashboard"></i> <span>Companies</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="<?php echo base_url(); ?>Applicant/notifications">
-                        <i class="fa fa-bell-o"></i> <span>Notifications</span>
-                    </a>
-                </li>
-            </ul>
-        </section>
-        <!-- /.sidebar -->
-    </aside>
 
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <h1>
-            My Profile
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Profile</li>
-        </ol>
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-
-        <div class="row">
-            <div class="col-md-3">
-
-                <!-- Profile Image -->
-                <div class="box box-primary">
-                    <div class="box-body box-profile">
-                        <a class="glyphicon glyphicon-pencil" role="button"></a>
-                        <img class="profile-user-img img-responsive img-square" src="#" alt="User profile picture" style="height: 200px;width: 200px">
-
-                        <h3  class="profile-username text-center"><b><?php echo $username?></b></h3>
-
-                        <h3 class="profile-username text-center"><?php echo $full_name?></h3>
-
-                        <p class="text-muted text-center">Software Engineer</p>
-                        <p class="text-muted text-center"><?php echo $dob ?></p>
-                        <p class="text-muted text-center"><?php echo $gender ?></p>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
-                <!-- /.box -->
-
-                <div class="box box-primary">
-                    <a class="glyphicon glyphicon-pencil" role="button"></a>
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Contact Details</h3>
-                    </div>
-                    <!-- /.box-header -->
-                    <div class="box-body">
-                        <strong><i class="fa fa-envelope margin-r-5"></i> Email</strong>
-=======
             <!-- /.box -->
 
             <div class="box box-primary">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Contact Details</h3>
+                    <h3 class="box-title">Contact Details</h3>  <a class="glyphicon glyphicon-pencil" role="button"></a>
                 </div>
                 <!-- /.box-header -->
                 <div class="box-body">
                     <strong><i class="fa fa-envelope margin-r-5"></i> Email</strong>
->>>>>>> master
 
                     <p class="text-muted">
                         john@gmail.com
