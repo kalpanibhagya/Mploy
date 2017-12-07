889a6dbc399d6cd7ba5c533a7a1c5c0a8551ffaa
janithR <rumalkajanith@gmail.com>
2017-12-07 14:52:41 +0530

    Change edit symbol

--- application/views/Pages/Applicant/profile.php
+++ application/views/Pages/Applicant/profile.php
@@ -22,7 +22,7 @@
             <div class="box box-primary">
                 <div class="box-body box-profile">
 
-                    <a class="glyphicon glyphicon-pencil" href="javascript:void(0)" role="button" onclick="edit_person()"></a>
+                    <a class="glyphicon glyphicon-plus" href="javascript:void(0)" role="button" onclick="edit_person()"></a>
                     <img class="profile-user-img img-responsive img-square" src="#" alt="User profile picture" style="height: 200px;width: 200px">
 
                     <h3  class="profile-username text-center"><b><?php echo $username?></b></h3>
@@ -171,7 +171,7 @@
 
             <div class="box box-primary">
                 <div class="box-header with-border">
-                    <a class="glyphicon glyphicon-pencil" href="javascript:void(0)" role="button" onclick="edit_person_contact()"></a>
+                    <a class="glyphicon glyphicon-plus" href="javascript:void(0)" role="button" onclick="edit_person_contact()"></a>
                     <h3 class="box-title">Contact Details</h3>
                 </div>
                 <!-- /.box-header -->
