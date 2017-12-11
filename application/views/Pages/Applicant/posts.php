bc19c4959e5bab8f4a5376abbe9625e16ad85c1e
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-12-11 15:20:11 +0530

    Fix error

--- application/views/Pages/Applicant/posts.php
+++ application/views/Pages/Applicant/posts.php
@@ -28,7 +28,7 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <form class="navbar-form navbar-right">
-                    <a class="btn btn-success" href="<?php echo base_url(); ?>Applicant/dashboard" role="button">Dashboard</a>
+                    <a class="btn btn-success" href="<?php echo base_url(); ?>Applicant/enter" role="button">Dashboard</a>
                     <a class="btn btn-success" href="<?php echo base_url(); ?>Applicant/logout" role="button">Logout</a>
                 </form>
             </div>
