cca40a630187afc19c840d6e2b506416cf274497
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-12-11 13:38:42 +0530

    Add minior ui issues

--- application/views/Pages/Company/content.php
+++ application/views/Pages/Company/content.php
@@ -8,20 +8,18 @@
     <div id="sliderFrame" style="margin-bottom: 20px">
         <div id="slider">
             <a href="" target="_blank">
-                <img src="<?php echo base_url('assets/images/programmer.jpg'); ?>" alt="#cap1" />
+                <img src="<?php echo base_url('assets/images/cover_bg_3.jpg'); ?>" alt="#cap1" />
+                <img src="<?php echo base_url('images/e.png'); ?>" class="img-rounded" alt="Find the best employees for your company" width="304" height="236">
             </a>
-            <img src="<?php echo base_url('images/la.jpg'); ?>" class="img-rounded" alt="Laboratory" width="304" height="236">
 
-            <img id="image1" src="<?php echo base_url('assets/images/welcomeMploy.jpg'); ?>" alt="Welcome to Our Class."/>
-            <img id="image1" src="<?php echo base_url('assets/images/field.jpg'); ?>" alt="Many type of classes."/>
-            <img id="image1" src="<?php echo base_url('assets/images/canteen.jpg'); ?>" />
+
+            <img id="image1" src="<?php echo base_url('assets/images/img.jpg'); ?>" alt="Your own Evaluation Criteria"/>
+            <img id="image2" src="<?php echo base_url('assets/images/3.png'); ?>" alt="Send Interview Requests"/>
+            <img id="image3" src="<?php echo base_url('assets/images/d.jpg'); ?>" />
         </div>
         <div style="display: none;">
             <div id="cap1">
                 Welcome to <a href="http://www.google.com/">M-ploy</a>.
-            </div>
-            <div id="cap2">
-                <em>HTML</em> caption. Link to <a href="http://www.google.com/">Google</a>.
             </div>
         </div>
     </div>
